7a3f3ab057c8911d19aee00812e4a9a94afe2a64
Sudhakar <bssr93@gmail.com>
2022-02-16 13:06:53 +0530

    data updated in Retrievefile

--- src/com/Lockedme/RetreiveFiles/RetrieveFile.java
+++ src/com/Lockedme/RetreiveFiles/RetrieveFile.java
@@ -1,7 +1,30 @@
 package com.Lockedme.RetreiveFiles;
 
+import java.io.File;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class RetrieveFile {
-	public void RetrieveFileMethod() {
-		System.out.println("11");
+	public void RetrieveFileMethod() //throws null 
+	{
+	Scanner user = new Scanner(System.in);
+	try
+	{
+//	File folders = new File(user.nextLine());
+	File folders = new File("D:\\Simplilearn");
+	if(folders.isDirectory());
+	{
+		File[] files = folders.listFiles();
+		
+		for (int i = 0; i < files.length; i++)
+		{			
+			System.out.println(i);
+		}
+	}
+	}
+	catch (NoSuchElementException e) {
+		System.out.println("Please Enter Valid User");
+	}
+	user.close();
 }
 }
--- src/com/Lockedme/RetreiveFiles/SearchFile.java
+++ src/com/Lockedme/RetreiveFiles/SearchFile.java
@@ -1,7 +1,12 @@
 package com.Lockedme.RetreiveFiles;
 
+import java.io.File;
+import java.util.Scanner;
+
+
 public class SearchFile {
 	public void SearchFileMethod() {
-		System.out.println("12");
+		
 }
+	
 }
